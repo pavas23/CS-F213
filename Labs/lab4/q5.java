0f8535f30eddc4e2d8f249300103e1b5cd9774ab
Pavas Garg <pavasgarg2003@gmail.com>
2022-09-24 09:49:58 +0530

    Add files via upload

---
+++ Labs/lab4/q5.java
@@ -0,0 +1,37 @@
+class Box{
+	int length,height,width;
+	Box(){
+		length = 10;
+		width = 5;
+		height = 2;
+	}
+	Box(int x,int y,int z){
+		length = x;
+		width = y;
+		height = z;
+	}
+	void printBoxDetails(){
+		System.out.println("Box length is: "+length);
+		System.out.println("Box width is: "+width);
+		System.out.println("Box height is: "+height);
+	}
+	public int volume(){
+		return length*width*height;
+	}
+	public void setDimensions(int x, int y, int z){
+		length = x;
+		width = y;
+		height = z;
+	}
+}
+public class q5{
+	public static void main(String[] args){
+		Box b1 = new Box(1,2,3);
+		b1.printBoxDetails();
+		int volume = b1.volume();
+		System.out.println("The volume of the box is: "+volume);
+		b1.setDimensions(1,1,1);
+		b1.printBoxDetails();
+		
+	}
+}
