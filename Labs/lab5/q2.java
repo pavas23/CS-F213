c7b1ed14d51507d05ec88da99d2c167334d2f712
Pavas Garg <pavasgarg2003@gmail.com>
2022-09-30 13:08:45 +0530

    Add files via upload

---
+++ Labs/lab5/q2.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+class 	FirstFrame extends Frame{
+	Button b1,b2;
+	public FirstFrame(){
+		addWindowListener(new WL());
+	}
+	class WL extends WindowAdapter{
+		public void windowClosing(WindowEvent e){
+			System.out.println("Window Closing");
+			System.exit(0);
+		}
+	}
+	public static void main(String[] args){
+		Frame f = new FirstFrame();
+		f.setSize(200,300); // (w,h);
+		f.setVisible(true);
+	}
+}
+
